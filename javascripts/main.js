4bc5e438b03998faa20701d4f5e4f76b5f387c13
Waamber <amberastuart@aol.com>
2017-10-24 19:37:42 -0500

    added review event

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,4 @@ const events = require('./events');
 const apiKeys = require('./apiKeys');
 
 apiKeys.retrieveKeys();
-events.myLinks();
-events.googleAuth();
-events.pressEnter();
-events.wishListEvents();
-
+events.init();
